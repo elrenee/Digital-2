91ec01cbfafa543c1a4856a45c7d805a9c464575
elrenee <gon23365@uvg.edu.gt>
2026-01-31 21:28:44 -0600

    Segundo POT
    Se agrego una logica para saber si se lee el primer o segundo potenciometro, ademas de que se corrio 6 bits a la derecha la lectura correspondiente al pot 2 ya que tengo ajustado a la Izquierda el ADC,

--- Lab02_LCD/Lab02_LCD/main.c
+++ Lab02_LCD/Lab02_LCD/main.c
@@ -15,10 +15,13 @@
 #include <stdlib.h>
 #include "PantallasLCD/LCD.h"
 #include "Convertidor/ADC.h"
+int adcleido=0;
 uint8_t lectura =1;
-uint8_t valor1= 0;
+uint8_t valor1= 0; //de 0 a 255
+uint16_t valor2=0; //lectura 2, de 0 a 1023
 float voltaje1=0;
 char buffer[10];
+char buffer2[10];
 /****************************************/
 // Function prototypes
 void setup();
@@ -28,17 +31,34 @@ void setup();
 int main(void)
 {
 	setup();
-	LCDcaracter('P');
-	LCDcaracter('1');
-	LCDsetcursor(1,2);
+	LCDstring("S1:");
+	LCDsetcursor(8,1);
+	LCDstring("S2");
 	while(1)
 	{
-		_delay_ms(10);
-		LCDsetcursor(1,2);
-		voltaje1= ((float)(valor1*5.00f)/255.0f);
-		dtostrf(voltaje1, 4, 2, buffer);
-		LCDstring(buffer);// cambiarlo a Entero
-		ADCSRA |=(1<<ADSC);
+		while (adcleido==1)//creo que no es necesario pero esto hara entrar a cambiar los valores desplegados en la LCD solo si hay lectura de ADC 
+		{
+			if (lectura==1)
+			{
+				LCDsetcursor(1,2);//lleva el cursor de escribir a la columna 1 fila 2 de mi pantalla
+				voltaje1= ((float)(valor1*5.00f)/255.0f);//convierte a float y cambia de 0-255 a 0-5 V 
+				dtostrf(voltaje1, 4, 2, buffer);
+				LCDstring(buffer);// escribo el valor en cadena
+				LCDcaracter('V');
+				ADMUX |= (1<<MUX0); //Cambiamos canal de lectura a Canal 1 del adc
+				lectura=2; // que pot leere en la proxima conversion?
+				ADCSRA |=(1<<ADSC);
+			}else if (lectura==2)
+			{
+				LCDsetcursor(7,2);
+				dtostrf(valor2, 4 ,0,buffer2);
+				LCDstring(buffer2);
+				ADMUX &= ~(1<<MUX0); //canal 0 seleccionado
+				lectura=1; //prox lectura sera en el 1
+				ADCSRA |= (1<<ADSC);
+			}
+			adcleido=0;
+		}
 	}
 }
 
@@ -62,17 +82,13 @@ void setup()
 //Interrupt subroutines
 ISR(ADC_vect)
 {
+	adcleido=1;
 	if (lectura==1)
 	{
 		valor1=ADCH;
-		//ADMUX |= (1<<MUX0); //Canal 1
-		//lectura=2;
-		
-		
 	}
-	//else if (lectura==2)
-	//{
-	//	valor2= ADCH;
-	//	ADMUX &=
-	//}
+	else if (lectura==2)
+	{
+		valor2= ADC>>6; // de 0 a 1023, 10 bits 
+	}
 }
