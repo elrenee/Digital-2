be9cda354375340a431b2adbef2d597ff19ee68c
elrenee <gon23365@uvg.edu.gt>
2026-02-07 02:15:28 -0600

    Interrupccion y comunicacion SPI
    Se configuro la interrupcion de los ADC´s para ir leyendo un pot y luego el otro sucesivamente.
    
    ademas se configuro la ISR para recibir los comandos que el maestro enviara para leer los valores de cada pot, con identificadores como 'a' o 'b' y tambien para recibir el dato que el usuario desea ver en el contador de leds.

--- Lab03_Slave/Lab03_Slave/main.c
+++ Lab03_Slave/Lab03_Slave/main.c
@@ -17,7 +17,9 @@
 uint8_t pot1=0;
 uint8_t pot2=0;
 uint8_t lectura=0;
-
+uint8_t spivalor=0;
+uint8_t contador=0;
+uint8_t hablamaestro=0;
 /****************************************/
 // Function prototypes
 void setup();
@@ -29,6 +31,7 @@ int main(void)
 	setup();
 	while(1)
 	{
+		PORTD = contador;
 		
 	}
 }
@@ -38,12 +41,58 @@ int main(void)
 void setup()
 {
 	cli();
+	DDRD = 0XFF; // PUERTO D COMO SALIDA
+	PORTD=0X00; //iNICIALMENTE APAGADOS
 	initSlave();
 	initADC();
 	SPCR|= (1<<SPIE);
+	ADCSRA|= (1<<ADSC);
 	sei();
 }
 
 
 /*******************************/
 //Interrupt subroutines
+ISR(ADC_vect)
+{
+	if (lectura==0)
+	{
+		pot1=ADCH;
+		lectura=1;
+		ADMUX|= (1<<MUX0); //CANAL 1 SELECCIONADO
+		ADCSRA|= (1<<ADSC);
+	}else if (lectura==1)
+	{
+		pot2=ADCH;
+		lectura=0;
+		ADMUX&= ~(1<<MUX0); //CANAL 0 SELECCIONADO
+		ADCSRA|=(1<<ADSC);
+	}
+}
+
+ISR(SPI_STC_vect)
+{
+	uint8_t spivalor= SPDR;
+	
+	if (hablamaestro==0)
+	{
+		if (spivalor== 'a')
+		{
+			SPDR=pot1;
+		}
+		else if (spivalor == 'b')
+		{
+			SPDR=pot2;
+		}
+		else if (spivalor=='c')
+		{
+			SPDR=0x00;
+			hablamaestro=1;
+		}
+	}else if (hablamaestro==1)
+	{
+		hablamaestro=0;
+		contador=spivalor;
+	}
+	
+}
